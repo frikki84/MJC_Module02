d29ba5ad4c895e6bbab659a676dcd7678ef6a5d2
Yana <Yana@mail.ru>
2021-01-22 11:50:30 +0300

    update .gitignore

--- src/main/java/com/epam/esm/dao/impl/CertificateTagDaoImpl.java
+++ src/main/java/com/epam/esm/dao/impl/CertificateTagDaoImpl.java
@@ -2,13 +2,10 @@ package com.epam.esm.dao.impl;
 
 import com.epam.esm.dao.CertificateTagDao;
 import com.epam.esm.entity.CertificateWithTagFromDb;
-import com.epam.esm.entity.GiftCertificateDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.CallableStatementCreator;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.SqlParameter;
-import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -22,10 +19,10 @@ import java.util.Map;
 @Component
 public class CertificateTagDaoImpl implements CertificateTagDao {
     public static final String SQL_QUERY_INSERT_CERTIFICATE_TAG_RELATION = "insert into gift_certificate_has_tag (gift_certicicate_id_gift_certicicate, tag_id_tag) values (?, ?) ";
-    public static final String SQL_QUERY_DELETE_CERTIFICATE_TAG_RELATION = "insert into gift_certicicate_has_tag (gift_certicicate_id_gift_certicicate, tag_id_tag) values (?, ?) ";
     public static final String SQL_QUERY_FIND_ALL_CERTIFICATES_WITH_TAGS = "select gc.*, t.* from gift_certificate gc join gift_certificate_has_tag gct on gc.id=gct.gift_certicicate_id_gift_certicicate join tag t on t.id=gct.tag_id_tag order by gc.id;";
     public static final String DB_PROCEDURE_CALL = "call DescriptionNameSearch(?);";
     public static final String MAP_KEY_NAME_PROCEDURE = "#result-set-1";
+
     private final JdbcTemplate template;
 
     @Autowired
@@ -38,7 +35,6 @@ public class CertificateTagDaoImpl implements CertificateTagDao {
         template.update(SQL_QUERY_INSERT_CERTIFICATE_TAG_RELATION, certificateId, tagId);
 
     }
-
     @Override
     public List<CertificateWithTagFromDb> findAllCertificatesWithTagsFromDb() {
         List<CertificateWithTagFromDb> resultList = template.query(
@@ -77,7 +73,6 @@ public class CertificateTagDaoImpl implements CertificateTagDao {
             ct.setLastUpdateDate(((Timestamp)c.get("last_update_date")).toLocalDateTime());
             //ct.setTagId((Long) c.get(8));
             ct.setTagName((String) c.get("nameTag"));
-            System.out.println("item: " + ct);
             certificateList.add(ct);
         });
          return certificateList;
@@ -86,7 +81,6 @@ public class CertificateTagDaoImpl implements CertificateTagDao {
 
     private Map<String, Object> searchCertificatesWithTags(String namePart) {
         List<SqlParameter> parameters = Arrays.asList(new SqlParameter(Types.NVARCHAR));
-
         return template.call(new CallableStatementCreator() {
             @Override
             public CallableStatement createCallableStatement(Connection con) throws SQLException {
